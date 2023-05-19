6d0a0b8cf60ec80e740fefc8920e04a67bc314fd
Mahan Raj Banerjee <mahan@sankhyasutralabs.com>
2023-05-19 15:19:02 +0530

    adding matrix block

---
+++ dep/include/matrix_block.hpp
@@ -0,0 +1,111 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+template<typename T, std::size_t D>
+class block
+{
+protected:
+    /* cuda block data */
+    std::size_t m_block_size[D];
+    std::size_t m_pad_size[D];
+    std::size_t m_block_size_padded[D];
+    std::size_t m_min_index[D];
+    std::size_t m_min_index_padded[D];
+    std::size_t m_max_index[D];
+    std::size_t m_max_index_padded[D];
+    
+    // std::size_t m_size;
+    // std::size_t m_size_padded; 
+    std::size_t m_bidx;
+    std::vector<std::size_t> m_nn_list = std::vector<std::size_t>(6);
+    T* m_data;
+
+public:
+    block ();
+    
+    block (std::size_t* t_block_size, 
+           std::size_t* t_pad_size);
+    
+    std::vector<std::size_t> 
+    get_block_size() const;
+
+    std::vector<std::size_t> 
+    get_block_size_padded() const;
+
+    std::size_t* 
+    get_zone_min();
+
+    std::size_t* 
+    get_zone_max();
+
+    std::size_t 
+    tindx  (std::size_t x, std::size_t y, std::size_t z);
+
+    // __device__ inline std::size_t 
+    // bidx (std::size_t bx, std::size_t by, std::size_t bz);
+
+    // void allocate();
+
+    void 
+    allocate();
+
+    void 
+    fill_boundary_px(const block<T,D> &t_block);
+
+    void 
+    fill_boundary_py(const block<T,D> &t_block);
+
+    void 
+    fill_boundary_pz(const block<T,D> &t_block);
+
+    void 
+    fill_boundary_mx(const block<T,D> &t_block);
+
+    void 
+    fill_boundary_my(const block<T,D> &t_block);
+
+    void 
+    fill_boundary_mz(const block<T,D> &t_block);
+
+    void 
+    fill(const T &t_val);
+
+    void 
+    fill_padded(const T &T_val);
+
+    T* 
+    data();
+
+    const T& 
+    operator[] (std::size_t idx) const;
+    
+    T& 
+    operator[] (std::size_t idx);
+
+    const T& 
+    operator() (std::size_t i, std::size_t j, std::size_t k) const;
+    
+    T& 
+    operator() (std::size_t i, std::size_t j, std::size_t k);
+
+    std::pair<std::size_t, std::size_t> 
+    size_of();
+
+    void 
+    dealloc();
+
+    void 
+    set_bidx(const std::size_t t_bidx);
+
+    void 
+    set_nn_blocks(const std::vector<std::size_t>& t_nn_list);
+
+    std::size_t 
+    get_bidx();
+
+    std::vector<std::size_t> 
+    get_nn_blocks();
+    
+    ~block ();
+};
---
+++ dep/src/matrix_block.cpp
@@ -0,0 +1,119 @@
+#include "block.ipp"
+
+template
+block<double,3>::block();
+
+template
+block<double,3>::block(std::size_t* t_block_size, 
+                  std::size_t* t_pad_size);
+
+template
+block<double,3>::~block();
+
+template 
+std::vector<std::size_t> 
+block<double,3>::get_block_size() const;
+
+template 
+std::vector<std::size_t> 
+block<double,3>::get_block_size_padded() const;
+
+template 
+std::size_t*
+block<double,3>::get_zone_min(); 
+
+template 
+std::size_t* 
+block<double,3>::get_zone_max(); 
+
+template
+std::size_t 
+block<double,3>::tindx(std::size_t x, std::size_t y, std::size_t z);
+
+template
+void 
+block<double,3>::allocate();
+
+template
+void 
+block<double,3>::fill_boundary_px(const block<double,3> &t_block);
+
+template
+void 
+block<double,3>::fill_boundary_py(const block<double,3> &t_block);
+
+template
+void 
+block<double,3>::fill_boundary_pz(const block<double,3> &t_block);
+
+template
+void 
+block<double,3>::fill_boundary_mx(const block<double,3> &t_block);
+
+template
+void 
+block<double,3>::fill_boundary_my(const block<double,3> &t_block);
+
+template
+void 
+block<double,3>::fill_boundary_mz(const block<double,3> &t_block);
+
+template
+void 
+block<double,3>::fill(const double &t_val);
+
+template 
+void 
+block<double,3>::fill_padded(const double &t_val);
+
+template 
+double *
+block<double,3>::data();
+
+template 
+const double& 
+block<double,3>::operator[] (std::size_t idx) const;
+
+template 
+double& 
+block<double,3>::operator[] (std::size_t idx);
+
+template 
+const double& 
+block<double,3>::operator() (std::size_t i, std::size_t j, std::size_t k) const;
+
+template 
+double& 
+block<double,3>::operator() (std::size_t i, std::size_t j, std::size_t k);
+
+template
+std::pair<std::size_t, std::size_t> 
+block<double,3>::size_of();
+
+template 
+void 
+block<double,3>::dealloc();
+
+template 
+void 
+block<double,3>::set_bidx(const std::size_t t_bidx);
+
+template 
+void 
+block<double,3>::set_nn_blocks(const std::vector<std::size_t>& t_nn_list);
+
+template 
+std::size_t 
+block<double,3>::get_bidx();
+
+template 
+std::vector<std::size_t> 
+block<double,3>::get_nn_blocks();
+
+/*
+int main(int argc, char** argv)
+{
+    block<double1,3> cuda_block(4,4,4,1,1,1);
+}
+*/
+
